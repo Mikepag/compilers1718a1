36feb1538ddf915781cca24f26047d58ad50f516
Mikepag <p14pagk@ionio.gr>
2018-03-21 01:00:21 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,10 +1,5 @@
-#http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
 
-
-
-#opening external file to save LOG
-logfile = open("log_file.txt", "w")
-
+logfile = open("log_file.txt", "w")	#Creates an external file to save program's input & output (log)
 
 def getchar(words,pos):
 	""" returns char at pos of words, or None if out of bounds """
@@ -37,15 +32,13 @@ def scan(text,transition_table,accept_states):
 
 			# check if current state is accepting
 			if state in accept_states:
-				return accept_states[state],pos
+				return accept_states[state],pos 	#if current state is accepting, scan() returns it.
 
 			# current state is not accepting
-			return 'ERROR_TOKEN',pos
+			return 'ERROR_TOKEN',pos 	#if current state is not accepting, scan() returns 'ERROR_TOKEN'.	
 			
 	
-# the transition table, as a dictionary
-
-# Αντικαταστήστε με το δικό σας λεξικό μεταβάσεων...
+# the transition table, as a dictionary:
 td = { 'q0':{ '0':'q1', '1':'q1', '2':'q6', '3':'q2', '4':'q2', '5':'q2', '6':'q2', '7':'q2', '8':'q2', '9':'q2' },
        'q1':{ '0':'q2', '1':'q2', '2':'q2', '3':'q2', '4':'q2', '5':'q2', '6':'q2', '7':'q2', '8':'q2', '.':'q3', ':':'q3' },
        'q2':{ '.':'q3', ':':'q3' },
@@ -54,16 +47,13 @@ td = { 'q0':{ '0':'q1', '1':'q1', '2':'q6', '3':'q2', '4':'q2', '5':'q2', '6':'q
        'q6':{ '0':'q2', '1':'q2', '2':'q2', '3':'q2', '.':'q3', ':':'q3' }
      } 
 
-# the dictionary of accepting states and their
-# corresponding token
-
-# Αντικαταστήστε με το δικό σας λεξικό καταστάσεων αποδοχής...
+# the dictionary of accepting states and their corresponding token:
 ad = { 'q5':'TIME_TOKEN' }
 
 
 # get a string from input
-text = input('give some input>')
-logfile.write(text + "\n")
+text = input('give some input>')				#reads input from user
+logfile.write("INPUT:     " + text + "\n")		#writes users input in logfile
 
 
 # scan text until no more input
@@ -72,19 +62,18 @@ while text:	# that is, while len(text)>0
 	# get next token and position after last char recognized
 	token,position = scan(text,td,ad)
 	
-	if token=='ERROR_TOKEN':
-		print('unrecognized input at pos',position+1,'of',text)
-		logfile.write('unrecognized input at pos ' + position+1 + 'of ' + text)
+	if token=='ERROR_TOKEN':															#if token=='ERROR_TOKEN', the user's input was not acceptable...
+		print('unrecognized input at pos',position+1,'of',text)							#... so we print (in terminal) that there was an unrecognized input and the position of the not acceptable character...
+		logfile.write("OUTPUT:    " + "unrecognized input at pos " + str(position+1) + " of " + text)	#...and we also write it in the logfile. (str() converts integer to string)
 
 		break
 	
-
-	print("token:",token,"string:",text[:position])
-	logfile.write("token: " + token + " string: " + text[:position] + "\n")
+																								#if token is not equal to 'ERROR_TOKEN', the user's input was acceptable!...
+	print("token:",token,"string:",text[:position])												#...so we print (in terminal) the token (TIME_TOKEN) and user's input...
+	logfile.write("OUTPUT:    " + "token: " + token + " string: " + text[:position] + "\n")		#...and we also write it in the logfile.
 
 	# remaining text for next scan
 	text = text[position:]
 
 
 logfile.close()
-	
