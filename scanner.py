845568f703b732807bec0887213730986bba4d70
Mikepag <p14pagk@ionio.gr>
2018-03-21 01:11:25 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -37,7 +37,7 @@ def scan(text,transition_table,accept_states):
 			# current state is not accepting
 			return 'ERROR_TOKEN',pos 	#if current state is not accepting, scan() returns 'ERROR_TOKEN'.	
 			
-	
+			
 # the transition table, as a dictionary:
 td = { 'q0':{ '0':'q1', '1':'q1', '2':'q6', '3':'q2', '4':'q2', '5':'q2', '6':'q2', '7':'q2', '8':'q2', '9':'q2' },
        'q1':{ '0':'q2', '1':'q2', '2':'q2', '3':'q2', '4':'q2', '5':'q2', '6':'q2', '7':'q2', '8':'q2', '.':'q3', ':':'q3' },
@@ -52,7 +52,7 @@ ad = { 'q5':'TIME_TOKEN' }
 
 
 # get a string from input
-text = input('give some input>')				#reads input from user
+text = input('give some input>')			#reads input from user
 logfile.write("INPUT:     " + text + "\n")		#writes users input in logfile
 
 
@@ -62,14 +62,13 @@ while text:	# that is, while len(text)>0
 	# get next token and position after last char recognized
 	token,position = scan(text,td,ad)
 	
-	if token=='ERROR_TOKEN':															#if token=='ERROR_TOKEN', the user's input was not acceptable...
-		print('unrecognized input at pos',position+1,'of',text)							#... so we print (in terminal) that there was an unrecognized input and the position of the not acceptable character...
+	if token=='ERROR_TOKEN':										#if token=='ERROR_TOKEN', the user's input was not acceptable...
+		print('unrecognized input at pos',position+1,'of',text)						#... so we print (in terminal) that there was an unrecognized input and the position of the not acceptable character...
 		logfile.write("OUTPUT:    " + "unrecognized input at pos " + str(position+1) + " of " + text)	#...and we also write it in the logfile. (str() converts integer to string)
 
 		break
-	
-																								#if token is not equal to 'ERROR_TOKEN', the user's input was acceptable!...
-	print("token:",token,"string:",text[:position])												#...so we print (in terminal) the token (TIME_TOKEN) and user's input...
+													#if token is not equal to 'ERROR_TOKEN', the user's input was acceptable!...
+	print("token:",token,"string:",text[:position])							#...so we print (in terminal) the token (TIME_TOKEN) and user's input...
 	logfile.write("OUTPUT:    " + "token: " + token + " string: " + text[:position] + "\n")		#...and we also write it in the logfile.
 
 	# remaining text for next scan
